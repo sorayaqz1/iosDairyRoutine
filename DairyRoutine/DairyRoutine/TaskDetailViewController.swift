4a4632bb32d6c455ee358ff683e76c1406d0b0e4
Qing Zhang <qzhang@qings-air.lan>
2018-05-24 14:15:35 -0500

    handle cancel on add item and edit existing item panel

--- DairyRoutine/DairyRoutine/TaskDetailViewController.swift
+++ DairyRoutine/DairyRoutine/TaskDetailViewController.swift
@@ -47,7 +47,18 @@ class TaskDetailViewController: UIViewController, UITextFieldDelegate, UIImagePi
     // MARK: - Navigation
 
     @IBAction func cancel(_ sender: UIBarButtonItem) {
-        dismiss(animated: true, completion: nil)
+        // Depending on style of presentation (modal or push presentation), this view controller needs to be dismissed in two different ways.
+        let isPresentingInAddMealMode = presentingViewController is UINavigationController
+        
+        if isPresentingInAddMealMode {
+            dismiss(animated: true, completion: nil)
+        }
+        else if let owningNavigationController = navigationController {
+            owningNavigationController.popViewController(animated: true)
+        }
+        else {
+            fatalError("The TaskViewController is not inside a navigation controller.")
+        }
     }
     
     
