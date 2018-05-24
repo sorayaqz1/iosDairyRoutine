82315a384b1da5bb81650411a7dc6cf49582411a
Qing Zhang <qzhang@qings-air.lan>
2018-05-24 11:30:03 -0500

    handle cancel button, dismiss and go back to list view when click

--- DairyRoutine/DairyRoutine/TaskDetailViewController.swift
+++ DairyRoutine/DairyRoutine/TaskDetailViewController.swift
@@ -37,11 +37,15 @@ class TaskDetailViewController: UIViewController, UITextFieldDelegate, UIImagePi
     }
     
 
-    /*
     // MARK: - Navigation
 
+    @IBAction func cancel(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+ /*   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
     }
